8163c89b4b94ff411306aa243dab74399cbb4a66
Emily Asch <e.asch@me.com>
2018-10-24 20:10:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,6 +5,7 @@ def get_first_name_of_season_winner(data, season)
       if v == "Winner"
         full_name = people["name"]
         return full_name.split(' ').first 
+      end
     end
   end 
 end
